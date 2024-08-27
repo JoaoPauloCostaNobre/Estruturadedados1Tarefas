933cf4252bf820f1ade390236ffb5cb5e2e6bcbc
JoaoPauloCostaNobre <158497878+JoaoPauloCostaNobre@users.noreply.github.com>
2024-08-27 08:38:31 -0300

    Melhorias PilhaDinamica

--- ParImpar/Main.java
+++ ParImpar/Main.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class PilhasDinamicas {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         DynamicStack pilhaPar = new DynamicStack();
